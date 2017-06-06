062cd76b9464f24f80ca7553ec404ffe6d0ec2b0
yamapi06150 <yamapi06150@gmail.com>
2017-06-06 23:54:41 +0800

    separate App from index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,8 @@
 // Import libraries to help create a component and render component to device screen
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/Header';
+import App from './src/components/App';
 
-// Create a Component
-const App = () => <Header headerText="Albums" />;
+
 
 // Render it to device screen
 AppRegistry.registerComponent('myFirstReactNativeApp', () => App);
